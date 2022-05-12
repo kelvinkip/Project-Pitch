d73afd48dfeecce373212f73769c29dd946d5e22
kelvinkip <kelvinkipla@gmail.com>
2022-05-12 03:42:12 +0300

    functions to run migration online and offline

--- manage.py
+++ manage.py
@@ -10,3 +10,38 @@ from flask import current_app
 from alembic import context
 
 config = context.config
+
+
+
+fileConfig(config.config_file_name)
+logger = logging.getLogger('alembic.env')
+
+
+config.set_main_option(
+    'sqlalchemy.url',
+    str(current_app.extensions['migrate'].db.engine.url).replace('%', '%%'))
+target_metadata = current_app.extensions['migrate'].db.metadata
+
+
+
+
+def run_migrations_offline():
+    
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+  
+
+    def process_revision_directives(context, revision, directives):
+        if getattr(config.cmd_opts, 'autogenerate', False):
+            script = directives[0]
+            if script.upgrade_ops.is_empty():
+                directives[:] = []
+                logger.info('No changes in schema detected.')
