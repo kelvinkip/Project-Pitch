0efc5fba7aa2f46b00db27e5126cd6e3775b86b4
kelvinkip <kelvinkipla@gmail.com>
2022-05-12 11:32:46 +0300

    general ,confuguration

---
+++ config.py
@@ -0,0 +1,37 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kelvin@localhost/pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SECRET_KEY = 'FlSkPItchA@*ppL&iCA^$tio***n'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
