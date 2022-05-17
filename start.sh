01737657fb454baba22cbad70e2230974bc8ed94
kelvinkip <kelvinkipla@gmail.com>
2022-05-17 20:54:00 +0300

    heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export MAIL_USERNAME='kelvin.kiplangat@student.moringaschool.com'
 export MAIL_PASSWORD='Kelvin.3098'
 SECRET_KEY ='FlSkPItchA@*ppL'
-python3.manage.py server
+python3 manage.py server
