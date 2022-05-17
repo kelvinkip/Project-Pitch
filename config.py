3fbeee351a7b4045aaf00b880506570dc33aa679
kelvinkip <kelvinkipla@gmail.com>
2022-05-17 22:23:06 +0300

    heroku

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config:
     General configuration parent class
 
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:run@localhost/pitch'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:run@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgres://zgjbwgpqyaetpj:f87addae0237f9ca08fdbf3da618f4e971ce0d9bb99a74a07c3aef4193f6b349@ec2-44-195-169-163.compute-1.amazonaws.com:5432/d8da1bo60058v'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY ='FlSkPItchA@*ppL'
     UPLOADED_PHOTOS_DEST = "app/static/photos" 
