8bf398363066f2da72458b55e833c573087bfcb4
kelvinkip <kelvinkipla@gmail.com>
2022-05-17 12:48:27 +0300

    changes

--- config.py
+++ config.py
@@ -4,29 +4,38 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = '<postgresql+psycopg2://moringa: ****@localhost/pitches>'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kelvin:1234@localhost/pitches'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SECRET_KEY = 'FlSkPItchA@*ppL&iCA^$tio***n'
-    #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
+    SECRET_KEY = os.environ.get('FlSkPItchA@*ppL')
+    UPLOADED_PHOTOS_DEST = "app/static/photos"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+
+    #email 
+    MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD  = os.environ.get("MAIL_PASSWORD")
+   
+
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-    
-
+    """
+    This is the class which we will use to set the configurations during production stage of the app
     Args:
-        Config: The parent configuration class with General configuration settings
-    
-    '''
- 
+        Config - this is the parent config class from which we inherit its properties
+
+    """
+
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # uri = os.environ.get("DATABASE_URL")  # or other relevant config var
+    # if uri.startswith("postgres://"):
+    #     uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
+
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = '<postgresql+psycopg2://moringa: ****@localhost/pitches>'
     '''
     Development  configuration child class
     Args:
